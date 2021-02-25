c551d478983873896fe45fc8eb60f14966493236
iemong <kiyoshidainagon@gmail.com>
2021-02-26 00:17:55 +0900

    configをちょっといじる

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: 'src/my-element.ts',
-      formats: ['es']
-    },
-    rollupOptions: {
-      external: /^lit-element/
+    build: {
+        lib: {
+            entry: 'src/entry.ts',
+            name: 'myLib',
+            formats: ['es', 'umd']
+        },
+        rollupOptions: {
+            external: /^lit-element/
+        }
     }
-  }
 })
